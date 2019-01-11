986350ca1d8734645db1eb16791c11e34dad66f6
honghaoran <2229009854@qq.com>
2019-01-11 09:59:35 +0800

    ignore

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@project: crawl img tag source
+@author: xiaohong
+@time: 2019-01-07
+@feature: crawl image for every site/multi thread to download/
+"""
